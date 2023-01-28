aea71e3ecc4e69029838870498b85a9aae081a28
hanyong5 <hanyong5@naver.com>
2023-01-28 13:39:47 +0900

    20230128

---
+++ data.js
@@ -0,0 +1,12 @@
+export default [
+    {subject: "감자떡"},
+    {subject: "건미역"},
+    {subject: "고기만두"},
+    {subject: "군만두"},
+    {subject: "귤"},
+    {subject: "깍두기"},
+    {subject: "깻잎"},
+    {subject: "누룽지"},
+    {subject: "단무지"},
+
+ ]
